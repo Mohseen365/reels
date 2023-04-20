b2cf4f0301716b1ffd978c4d7b504039d8666cbc
Mohseen <Mohseen@appperfect.com>
2023-04-20 23:54:26 +0530

    Instagram Clone

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC3AZU4FgRC1ShrvWZ1HX_siSSOZjDFqWM",
+  authDomain: "reels-58d8b.firebaseapp.com",
+  projectId: "reels-58d8b",
+  storageBucket: "reels-58d8b.appspot.com",
+  messagingSenderId: "407701175983",
+  appId: "1:407701175983:web:98004200767b90fbc81e5a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
